6b5e94d7a32b113732f954cecef42e81f9fb6959
CIAg <euluishenrique7@gmail.com>
2024-05-28 08:17:44 -0300

    cors

--- src/repositorys/precoRepository.js
+++ src/repositorys/precoRepository.js
@@ -3,19 +3,18 @@ const connection = require('./connection');
 const getAll = async () => {
     const [preco] = await connection.execute('SELECT * FROM PRECO_PROFESSOR');
     return preco;
-}
+};
 
 const createPreco = async (id_professor, preco_minimo, preco_maximo) => {
     const query = "INSERT INTO PRECO_PROFESSOR (id_professor, preco_minimo, preco_maximo) VALUES (?,?,?)";
     const [createdPreco] = await connection.execute(query, [id_professor, preco_minimo, preco_maximo]);
     return { message: 'Criou o bagulhp'}
-}
+};
 
 const deletePreco = async (id_professor) => {
     const query = "DELETE FROM PRECO_PROFESSOR WHERE id_professor = ?";
     return await connection.execute(query, [id_professor]);
 };
-
 
 const updatePreco = async (id_professor, PRECO_PROFESSOR) => {
     const { preco_minimo, preco_maximo } = PRECO_PROFESSOR;
@@ -24,10 +23,9 @@ const updatePreco = async (id_professor, PRECO_PROFESSOR) => {
     return updatePreco;
 };
 
-
 module.exports = {
     getAll,
     createPreco,
     deletePreco,
     updatePreco,
-}
+};
