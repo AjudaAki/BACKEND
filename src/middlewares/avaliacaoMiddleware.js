45d0d277459c569e669864218aa2789be94462bd
Francieli Martins Rocha <fmartinsPOW777@gmail.com>
2024-06-05 10:09:22 -0300

    middleware avaliação

--- src/middlewares/avaliacaoMiddleware.js
+++ src/middlewares/avaliacaoMiddleware.js
@@ -1,3 +1,4 @@
+const connection = require('../repositories/connection');
 const { response } = require("express");
 
 const validateAvaliacao = (request, response, next) => {
@@ -8,32 +9,23 @@ const validateAvaliacao = (request, response, next) => {
     }
     request.notaNumber = notaNumber
     next();
-
 }
-
-const validateIdUsuario = (request, response, next) => {
-    const { body } = request;
-    const idLogado = request.userId; 
-
-    if (isNaN(body.usuario_avaliador) || parseInt(body.usuario_avaliador)) {
-        return response.status(400).json({ message: "Não é possível se autoavaliar!" });
-    }
-    next();
-};
 
 const validateAvaliacaoDuplicada = async (request, response, next) => {
     const { body } = request;
-    const { usuario_logado, usuario_relacionado } = body; 
+    const { usuario_avaliador, professor_avaliado } = body; 
     try {
         const query = "SELECT * FROM AVALIACAO_PROFESSOR WHERE usuario_avaliador = ? AND professor_avaliado = ?";
-        const [rows] = await pool.query(query, [usuario_logado, usuario_relacionado]);
-
+        const [rows] = await connection.execute(query, [usuario_avaliador, professor_avaliado]);
+    
         if (rows.length > 0) {
             return response.status(400).json({ message: "Apenas é permitido fazer uma avaliação para cada professor." });
         }
 
         next();
+
     } catch (error) {
+
         console.error("Erro ao verificar avaliação duplicada:", error);
         return response.status(500).json({ message: 'Erro interno do servidor' });
     }
@@ -41,6 +33,5 @@ const validateAvaliacaoDuplicada = async (request, response, next) => {
 
 module.exports = {
     validateAvaliacao,
-    validateIdUsuario,
     validateAvaliacaoDuplicada
 }
