48eed101de64d0a7283f2a9ed281b4e3679867de
CIAg <euluishenrique7@gmail.com>
2024-06-07 10:43:13 -0300

    app imagem

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors')
 const routerPrincipal = require('./routers/routerPrincipal');
 
 const app = express();
   
+app.use(bodyParser.json({ limit: '10mb' }));
+
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
 app.use(express.json());
 
 app.use(cors())
